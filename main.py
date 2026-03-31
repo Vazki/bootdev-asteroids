06aa26e7945374f8da257f45ea973d16627c0bb1
Vazki <vazki@vazki.dev>
2026-03-31 02:17:47 -0500

    added clock and limit fps

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        log_state()
         for event in pygame.event.get():
-            if event.type == pygame.q  QUIT:
+            if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
