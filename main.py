303bcdddda8b8475f10e575cde597c333ede8741
Vazki <vazki@vazki.dev>
2026-04-03 06:56:02 -0500

    we got movement

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from logger import log_state
 from player import Player
 
 def main():
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for u in updatable:
+            u.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
